351c28b1fdd6a14df7ed86f8874c639a24d8d935
ras <agalvezcorell@gmail.com>
2021-08-24 13:46:49 +0200

    updating

--- week_03/SQL_scripts/01-creating_database.sql
+++ week_03/SQL_scripts/01-creating_database.sql
@@ -44,9 +44,53 @@ phone_no VARCHAR(20)
 
 -- STEP 2: create the tables client, participants, course
 
+CREATE TABLE client(
+client_id INT PRIMARY KEY,
+client_name VARCHAR(40) NOT NULL,
+address VARCHAR(40) NOT NULL,
+industry VARCHAR(40)
+);
+
+CREATE TABLE participant (
+  participant_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  phone_no VARCHAR(20),
+  client INT
+);
+
+
+CREATE TABLE IF NOT EXISTS course(
+course_id INT PRIMARY KEY,
+course_name VARCHAR(40) NOT NULL,
+language VARCHAR (3) NOT NULL,
+level VARCHAR(2),
+course_lemgth_weeks INT,
+start_date DATE,
+in_school BOOLEAN,
+teacher INT,
+client INT
+);
+
 ###################################
 #      CREATE THE RELATIONHIPS    #
 ###################################
+
+ALTER TABLE participant
+ADD FOREIGN KEY(client)
+REFERENCES client(client_id)
+ON DELETE SET NULL; 
+
+ALTER TABLE course
+ADD FOREIGN KEY(teacher)
+REFERENCES teacher(teacher_id)
+ON DELETE CASCADE;
+
+ALTER TABLE course
+ADD FOREIGN KEY(client)
+REFERENCES client(client_id)
+ON DELETE SET NULL;
+
 
 
 
